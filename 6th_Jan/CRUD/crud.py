fbe0a818bdd2fac8598646d794017b77217be50d
AaryaBatkadli <aaryabatkadli@gmail.com>
2026-01-08 12:41:00 +0530

    Convert CRUD from submodule to normal folder

---
+++ 6th_Jan/CRUD/crud.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+import models, schemas
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_all_users(db: Session):
+    return db.query(models.User).all()
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def delete_user(db: Session, user_id: int):
+    user = get_user_by_id(db, user_id)
+    if user:
+        db.delete(user)
+        db.commit()
+    return user
